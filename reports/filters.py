b206d263bca616ba3075ca78f7c73489a2f238ee
jameswhitakerwork <jameswhitakerwork@gmail.com>
2017-05-07 21:17:37 +0800

    reports

--- reports/filters.py
+++ reports/filters.py
@@ -5,10 +5,20 @@ from hr.models import Program
 
 
 class ReportFilter(django_filters.FilterSet):
+    submitted_filter = BooleanFilter(
+        label = 'Submitted?,',
+        method='filter_submitted'
+    )
+
+    def filter_submitted(self, queryset, name, value):
+        value = 1 - value
+        return queryset.filter(report__isnull=value)
 
     class Meta:
         model = Report
         exclude = [
             'report',
             'deadline',
+            'reportee',
+            'name',
         ]
